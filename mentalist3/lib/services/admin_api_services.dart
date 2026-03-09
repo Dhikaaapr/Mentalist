52b84aa5d093aba692055026d7ea9ff5b9c1b8d6
dhika <andhika0143@gmail.com>
2026-03-09 15:49:39 +0700

    chore: update API base URL to Vercel production and cleanup logs

--- mentalist3/lib/services/admin_api_services.dart
+++ mentalist3/lib/services/admin_api_services.dart
@@ -11,7 +11,7 @@ class AdminApiService {
 
   // physical device
   // static const String baseUrl = 'http://10.92.142.43:8000/api';
-  static const String baseUrl = 'https://mentalist-ia6v6uchd-dhikaaaprs-projects.vercel.app/api';
+  static const String baseUrl = 'https://mentalist-one.vercel.app/api';
   static const Duration timeoutDuration = Duration(seconds: 30);
 
   /// -------------------------------
